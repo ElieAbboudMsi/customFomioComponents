97f7f82544a5e0000d91eac72db3a3906d180c8e
elie <elie.abboud@msiuk.com>
2023-09-13 16:55:52 +0300

    1

--- formsflow-custom-formio/customFormioComponents/index.js
+++ formsflow-custom-formio/customFormioComponents/index.js
@@ -1,13 +1,11 @@
 // Import any other new components or templates here.
 import TextAreaWithAnalytics from "./TextAreaWithAnalytics/TextAreaWithAnalytics";
-import TextAreaWithAnalytics from "./TextAreaWithAnalytics2/TextAreaWithAnalytics2";
 
 
 // Add imported components to the components object
 const components = {
-  textAreaWithAnalytics: TextAreaWithAnalytics,
-  textAreaWithAnalytics2: TextAreaWithAnalytics2
-};
+  textAreaWithAnalytics: TextAreaWithAnalytics
+  };
 
 export default {
   components : components
